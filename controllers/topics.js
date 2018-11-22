d66f01a7c4b4cde3995ffe38ea2fa606db21b013
brlom <brlom007@gmail.com>
2018-11-22 16:27:08 +0000

    working on testing queries for api/topics/:topic/articles

--- controllers/topics.js
+++ controllers/topics.js
@@ -1,14 +1,14 @@
 const db = require('../db/connection');
 
-// const validateQueries = (rawQueries, ...validQueries) => {
-//   return validQueries.reduce((filtered, query) => {
-//     if (rawQueries[query]) filtered[query] = rawQueries[query];
-//     return filtered;
-//   }, {});
-// };
+const validateQueries = (rawQueries, ...validQueries) => {
+  return validQueries.reduce((filtered, query) => {
+    if (rawQueries[query]) filtered[query] = rawQueries[query];
+    return filtered;
+  }, {});
+};
 
 exports.getAllTopics = (req, res, next) => {
-  db('topics')
+  return db('topics')
     .select()
     .then((topics) => {
       res.status(200).send({ topics });
@@ -17,30 +17,29 @@ exports.getAllTopics = (req, res, next) => {
 };
 
 exports.postNewTopic = (req, res, next) => {
-  db('topics')
+  return db('topics')
     .insert({ slug: req.body.slug, description: req.body.description })
     .returning('*')
     .then(([topic]) => {
       res.status(201).send({ topic });
     })
-    .catch(next)
+    .catch(next);
 };
 
 exports.getArticlesByTopic = (req, res, next) => {
+  const query = validateQueries(req.query);
   const { topic } = req.params;
-  db('topics')
-    .select()
-    .join('articles', 'topics.slug', '+', 'articles.topic')
-    .where('topics', topic)
+  const { limit, sortBy, order, page } = req.query;
+  return db('articles')
+    .select('article_id', 'title', 'votes', 'created_at', 'topic')
+    .join('topics', 'articles.topic', 'topics.slug')
+    .where('articles.topic', topic)
     .limit(limit || 10)
     .orderBy(sortBy || 'created_at', order || 'desc')
     .offset(page || 0)
+    .where(query)
     .then(([articles]) => {
       res.status(200).send({ articles });
     })
-    .catch(next)
-}
-
-// const query = validateQueries(req.query);
-// const { topic } = req.params;
-// const { limit, sortBy, page } = req.query;
+    .catch(next);
+};
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -33,17 +33,17 @@ describe('/api', () => {
     );
     it('POST returns 201 and new topic object', () => {
       const newTopic = {
-        'slug': 'chuckNorris',
-        'description': 'You don\'t find him, he find\'s you'
-      }
+        slug: 'chuckNorris',
+        description: 'You don\'t find him, he find\'s you',
+      };
       return request
         .post('/api/topics')
         .send(newTopic)
         .expect(201)
         .then(({ body }) => {
           expect(body.topic).to.be.an('object');
-          expect(body.topic).to.have.all.keys("slug", "description");
-        })
+          expect(body.topic).to.have.all.keys('slug', 'description');
+        });
     });
     it('GET returns 200 and an object of all topic articles', () => {
       return request
@@ -51,9 +51,8 @@ describe('/api', () => {
         .expect(200)
         .then(({ body }) => {
           expect(body).to.have.all.keys('articles');
-          expect(body.articles.length).to.equal(3);
-          expect(body.articles[0]).to.be.an('object');
-          expect(body.articles[0]).to.have.all.keys(
+          expect(body.articles).to.be.an('object');
+          expect(body.articles).to.have.all.keys(
             // 'author',
             'title',
             'article_id',
@@ -62,7 +61,28 @@ describe('/api', () => {
             'created_at',
             'topic'
           );
+        });
+    });
+    it('GET returns 200 and an object where responses are limited', () => {
+      const limit = 10;
+      return request
+        .get('/api/topics/cats/articles?limit=10')
+        .expect(200)
+        .then(({ body }) => {
+          const ol = Object.keys(body.articles);
+          expect(ol.length).to.be.below(limit);
+        });
+    });
+    it('GET returns 200 and an array of objects which is sorted by date', () => {
+      const firstArticleDesc = 'Living in the shadow of a great man';
+      const lastArticleDesc = 'Moustache';
+      return request
+        .get('/api/topics/mitch/articles?sortBy=created_at')
+        .expect(200)
+        .then((body) => {
+          expect(body.articles[0].title).to.equal(firstArticleDesc);
+          expect(body.articles[-1].title).to.equal(lastArticleDesc);
         })
     })
-  })
+  });
 });
