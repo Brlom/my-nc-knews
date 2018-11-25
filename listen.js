0bf50c66f96040959ea0ccadee72f6d98d2275cc
brlom <brlom007@gmail.com>
2018-11-25 17:10:49 +0000

    added heroku setup

--- listen.js
+++ listen.js
@@ -1,7 +1,11 @@
 const app = require('./app');
 
-const port = 9090;
+// const port = 9090;
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`); // eslint-disable-line no-console
-});
+// app.listen(port, () => {
+//   console.log(`listening on port ${port}`); // eslint-disable-line no-console
+// });
+
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
