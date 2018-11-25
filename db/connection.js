60e51de1fb91999e718628610e1ab20a2ce9f32d
brlom <brlom007@gmail.com>
2018-11-25 17:18:29 +0000

    fixed npm run start

--- db/connection.js
+++ db/connection.js
@@ -1,19 +1,4 @@
-// const ENV = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
-// const knex = require('knex');
-// const config = require('../knexfile');
-
-// const db = knex(
-//   config[ENV],
-// );
-
-// module.exports = db;
-
 const ENV = process.env.NODE_ENV || 'development';
 const config = ENV === 'production' ? { client: 'pg', connection: process.env.DATABASE_URL } : require('../knexfile')[ENV];
 
-const db = knex(
-  config[ENV],
-);
-
-module.exports = db;
 module.exports = require('knex')(config);
