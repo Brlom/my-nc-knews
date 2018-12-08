3cb56cff6df346bf43a7caded0e15dbabd76bff1
brlom <brlom007@gmail.com>
2018-12-08 17:12:36 +0000

    update migration to set votes to 0

--- db/migrations/20181208164730_non-nullable-votes.js
+++ db/migrations/20181208164730_non-nullable-votes.js
@@ -1,7 +1,9 @@
 exports.up = function (knex, Promise) {
-    return knex.schema.alterTable('articles', (articlesTable) => {
-        articlesTable.integer('votes').defaultTo(0).notNullable().alter();
-    })
+    return knex('articles').update('votes', 0)
+        .then(knex.schema.alterTable('articles', (articlesTable) => {
+            articlesTable.integer('votes').defaultTo(0).notNullable().alter();
+        }))
+
 };
 
 exports.down = function (knex, Promise) {
